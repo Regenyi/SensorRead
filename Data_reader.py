09167409c478ff0001ebb8d401be8d4d881c8e8a
regenyi <regenyi.adam@gmail.com>
2018-07-29 16:27:03 +0200

    data filtering 0.1

--- Data_reader.py
+++ Data_reader.py
@@ -1,17 +1,28 @@
 import csv
 import datetime
 
-filename = 'test01.csv'
+filename = 'test01.tsv'
 with open(filename) as ds:
-    reader = csv.reader(ds)  # , delimiter='\t')
+    reader = csv.reader(ds, delimiter='\t')
     data = [r for r in reader]
     # data = []
     # for r in row:
     #     data.append(r)
 
+filtered_data = []
 for row in data:
-    print(datetime.datetime.utcfromtimestamp(int(row[0])).strftime('%H:%M:%S'))
-    print(', '.join(row[2:4]))
-    # print(row)
+    #  print(datetime.datetime.utcfromtimestamp(int(row[0])).strftime('%Y-%m-%d %H:%M:%S'))
+    first_coord = row[2]
+
+    if first_coord[0] == "0":  # or first_coord[0:3] == "-0,0":
+        filtered_data.append("string")
+    else:
+        filtered_data.append(row)
+
+
+
+for row in filtered_data:
+    print(':: '.join(row[2:5]))
+
 
 
