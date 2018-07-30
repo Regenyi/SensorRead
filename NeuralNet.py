b5f92cf665d04300a08ab76ccf77e19f61aeb457
regenyi <regenyi.adam@gmail.com>
2018-07-30 16:08:52 +0200

    matrix on sample data loops through

--- NeuralNet.py
+++ NeuralNet.py
@@ -2,10 +2,11 @@ import csv
 import numpy as np
 import random
 
-
+x = 0
 file = "data/save-1.tsv"
 dat = []
-array1 = np.array([random.uniform(-1, 1) for _ in range(10)], np.float)
+result = []
+weight_random = np.array([random.uniform(-1, 1) for _ in range(10)], np.float)
 array2 = np.array([random.uniform(-1, 1) for _ in range(10)], np.float)
 layer1_con_weights = [[1.0, 2.0],
                       [3.0,  4.0],
@@ -25,7 +26,7 @@ def nonlin(x, deriv=False):
     return 1 / (1 + np.exp(-x))
 
 
-def readLines():
+def read_lines():
     with open(file) as data:
         reader = csv.reader(data, delimiter=' ')
         for row in reader:
@@ -36,6 +37,10 @@ def readLines():
             dat.append(temp)
 
 
+def array_randomizer():
+    return np.array([random.uniform(-1, 1) for _ in range(10)], np.float)
+
+
 def training_start():
     array1 = np.array([random.uniform(-1, 1) for _ in range(10)])
     array2 = np.array([random.uniform(-1, 1) for _ in range(10)])
@@ -43,31 +48,48 @@ def training_start():
     array4 = np.array([random.uniform(-1, 1) for _ in range(10)])
 
 
-def get_matrix():
+def get_matrix(line, rnd1, rnd2):
     for iter in range(1):
         # forward propagation
-        l0 = dat[0]
+        l0 = dat[line]
         # print("2x1" , np.dot(np.matrix([[2], [5], [1]]), np.matrix([[3, 2]])))
         # l1 = np.dot(np.matrix([[1], [5]]), np.matrix(array1))
+        # l1 = np.dot(np.matrix([[1, 5]]), np.matrix([weight_random, array2]))  # 10x2,  2x1
+        # l1 = nonlin(np.dot(np.matrix(layer1_con_weights), np.matrix([l0])))  # 2x10,  1x2
 
-        l1 = np.dot(np.matrix([[1, 5]]), np.matrix([array1, array2]))  # 10x2,  2x1
-        l1 = nonlin(np.dot(np.matrix(layer1_con_weights), np.matrix([[1], [4]])))  # 2x10,  1x2
-        output = nonlin(np.dot(np.matrix([array1, array2]), np.matrix(l1)))
+        l1 = nonlin(np.dot(np.matrix(layer1_con_weights), np.matrix([[l0[0]], [l0[1]]])))  # 2x10,  1x2
+        output = nonlin(np.dot(np.matrix([rnd1, rnd2]), np.matrix(l1)))
 
         # l1 = nonlin(np.dot(l0, array1))
-    print(output)
 
-# 2 inputhoz a 2 output szám elmentése 2d-s listába
+    result.append(output)
+
 
 def print_res():
     for row in dat:
         print(row)
 
 
-readLines()
-training_start()
-get_matrix()
-# print_res()
+def print_res2():
+    for row in range(len(dat)):
+        print(dat[row], result[row])
+
+
+def loop(x):
+    rnd1 = array_randomizer()
+    rnd2 = array_randomizer()
+    while x != len(dat):
+        line = 0
+        # training_start()
+        get_matrix(line, rnd1, rnd2)
+        line += 1
+        x += 1
+
+
+read_lines()
+loop(x)
+print_res2()
+
 
 
 
