2d9caefda29e6ddb49f2db0755df63ce496391c2
Regenyi <regenyi.adam@gmail.com>
2018-08-22 17:11:07 +0200

    - adding var 'shape' to the rest of the code

--- neural_net.py
+++ neural_net.py
@@ -53,8 +53,8 @@ def run_nn_on_line(input, nn):
 
 
 def run_nn_on_input_data(iter, nn):
-    if nn == 0:
-        nn = create_random_nn()
+    # if nn == 0:
+    #    nn = create_random_nn()
     list_of_nns[iter] = nn
     line = 0
     last_out = [0] * 22
@@ -66,6 +66,7 @@ def run_nn_on_input_data(iter, nn):
 
 
 def run_population(num, iter=0, nn=0):
+    # for itt
     if iter == 0:
         for i in range(num):
             run_nn_on_input_data(i, nn)
@@ -139,7 +140,7 @@ def should_be(a, b):
 
 
 def test():
-    should_be(len(create_random_nn([2, 2, 2])      ), 2)
+    should_be(len(create_random_nn([2, 3, 2])      ), 2)
     should_be(len(create_random_nn([2, 3, 2])[0]   ), 2)
     should_be(len(create_random_nn([2, 3, 2])[0][0]), 3)
     should_be(len(create_random_nn([2, 3, 2])[1]   ), 3)
@@ -155,10 +156,21 @@ def test():
     should_be(len(create_random_nn([2, 4, 4, 2])[2][0]), 2)
     # should_be(len(create_random_nn([2, 2, 6, 2])), 4)
 
+    should_be(len(create_random_nn([2, 3])      ), 1)
+    should_be(len(create_random_nn([2, 3])[0]   ), 2)
+    should_be(len(create_random_nn([2, 3])[0][0]), 3)
+
+    should_be(len(create_random_nn([2])      ), 0)
+
+    should_be(len(create_random_nn([])      ), 0)
+
+    print("\n*** All tests PASS. ***")
 
 def main():
     # *** INIT: *** #
     test()
+    # return
+
     logging.basicConfig(filename='log.txt', level=logging.DEBUG, format='%(message)s', filemode="w")
     # logging.basicConfig(level=logging.DEBUG, format='%(message)s')
     global processed_sensor_data
@@ -166,7 +178,8 @@ def main():
     file = "data/save-1.tsv"
     processed_sensor_data = data_processor.read_lines(file, ' ')
     create_empty_res_lists(10)
-    run_population(10)
+    nns = for c in 10 create_random_nn([123,123])
+    run_population(nns)
     biggest_num_and_source_nn_index_list = rank_nns()
     nextgen = breeder(biggest_num_and_source_nn_index_list)
 
